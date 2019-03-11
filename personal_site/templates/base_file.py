1cdee3cc2d34c1f6956ea2870e4be28bbf1d704d
Savagecoder77 <savagecoder77@gmail.com>
2019-03-11 13:54:25 -0700

    add meta data

--- personal_site/templates/base_file.py
+++ personal_site/templates/base_file.py
@@ -1,34 +1,53 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    {% block head_meta %}
-      <!-- Title & SEO -->
-      <meta name="keywords" content="rafael,cenzano,rafael cenzano,san francisco,hackthefog,hack the fog,marvin virtual assistant,random coders">
-      <meta name="image" content="{{ url_for('static', filename='img/profile.jpeg') }}">
-      <title>Rafael Cenzano</title>
-      <meta name="description" content="Lowell Help Forum News Page.">
-      <meta charset='utf-8'>
+<head>
+	<!-- Title -->
+	<title>{% block title %}{% endblock %}</title>
 
-      <!-- Viewport -->
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Meta Data -->
+	<meta charset="utf-8">
+	<meta name="robots" content="index,follow">
+	<meta name="theme-color" content="#ffffff">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta name="keywords" content="Rafael Cenzano">
+	<meta name="description" content="Website for Lowell High School's Dev Club.">
+	<meta name="msapplication-TileColor" content="#da532c">
 
-      <!-- Theming & Site Icons -->
-      <link rel="apple-touch-icon" sizes="180x180" href="{{ url_for('static', filename='img/apple-touch-icon.png') }}">
-      <link rel="icon" type="image/png" sizes="32x32" href="{{ url_for('static', filename='img/favicon-32x32.png') }}">
-      <link rel="icon" type="image/png" sizes="16x16" href="{{ url_for('static', filename='img/favicon-16x16.png') }}">
-      <link rel="mask-icon" href="{{ url_for('static', filename='img/safari-pinned-tab.svg') }}" color="#5bbad5">
-      <meta name="msapplication-TileColor" content="#da532c">
-      <meta name="theme-color" content="#ffffff">
-    {% endblock %}
+	<!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url_for('static', filename='img/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url_for('static', filename='img/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url_for('static', filename='img/favicon-16x16.png') }}">
+    <link rel="mask-icon" href="{{ url_for('static', filename='img/safari-pinned-tab.svg') }}" color="#5bbad5">
+
+	<!-- Facebook -->
+	<meta property="og:url" content="http://www.lowelldev.club">
+	<meta property="og:image" content="/static/img/High_Resolution_dev_club_logo.png">
+	<meta property="og:description" content="Website for Lowell High School's Dev Club.">
+	<meta property="og:title" content="Rafael Cenzano">
+	<meta property="og:site_name" content="Rafael Cenzano">
+	<meta property="og:see_also" content="http://www.lowelldev.club">
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="Rafael Cenzano">
+	<meta name="twitter:url" content="http://www.lowelldev.club">
+	<meta name="twitter:title" content="Rafael Cenzano">
+	<meta name="twitter:description" content="Website for Lowell High School's Dev Club.">
+	<meta name="twitter:image" content="/static/img/High_Resolution_dev_club_logo.png">
+
+	<!-- Google+ -->
+	<meta itemprop="name" content="Rafael Cenzano">
+	<meta itemprop="description" content="Website for Lowell High School's Dev Club.">
+	<meta itemprop="image" content="/static/img/High_Resolution_dev_club_logo.png">
+
     {% block head_css %}
-      <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='css/lib/bootstrap.min.css') }}">
     {% endblock %}
-    {% block head_js %}{% endblock %}
-  </head>
-  <body>
+    {% block head_js %}
+    {% endblock %}
+</head>
+<body>
     {% block content %}
     {% endblock %}
     {% block trailing_js %}
     {% endblock %}
-  </body>
+</body>
 </html>
