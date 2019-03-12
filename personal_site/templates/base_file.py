b7db24320d986e86719d625b0c2ea64c341cc7b4
Savagecoder77 <savagecoder77@gmail.com>
2019-03-11 18:48:41 -0700

    change meta data

--- personal_site/templates/base_file.py
+++ personal_site/templates/base_file.py
@@ -12,6 +12,8 @@
 	<meta name="keywords" content="Rafael Cenzano">
 	<meta name="description" content="Website for Lowell High School's Dev Club.">
 	<meta name="msapplication-TileColor" content="#da532c">
+	<link rel="author" href="http://rafaelcenzano.com">
+	<link rel="canonical" href="http://rafaelcenzano.me">
 
 	<!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="{{ url_for('static', filename='img/apple-touch-icon.png') }}">
@@ -19,26 +21,8 @@
     <link rel="icon" type="image/png" sizes="16x16" href="{{ url_for('static', filename='img/favicon-16x16.png') }}">
     <link rel="mask-icon" href="{{ url_for('static', filename='img/safari-pinned-tab.svg') }}" color="#5bbad5">
 
-	<!-- Facebook -->
-	<meta property="og:url" content="http://www.lowelldev.club">
-	<meta property="og:image" content="/static/img/High_Resolution_dev_club_logo.png">
-	<meta property="og:description" content="Website for Lowell High School's Dev Club.">
-	<meta property="og:title" content="Rafael Cenzano">
-	<meta property="og:site_name" content="Rafael Cenzano">
-	<meta property="og:see_also" content="http://www.lowelldev.club">
-
-	<!-- Twitter -->
-	<meta name="twitter:card" content="Rafael Cenzano">
-	<meta name="twitter:url" content="http://www.lowelldev.club">
-	<meta name="twitter:title" content="Rafael Cenzano">
-	<meta name="twitter:description" content="Website for Lowell High School's Dev Club.">
-	<meta name="twitter:image" content="/static/img/High_Resolution_dev_club_logo.png">
-
-	<!-- Google+ -->
-	<meta itemprop="name" content="Rafael Cenzano">
-	<meta itemprop="description" content="Website for Lowell High School's Dev Club.">
-	<meta itemprop="image" content="/static/img/High_Resolution_dev_club_logo.png">
-
+	{% block meta %}
+	{% endblock %}
     {% block head_css %}
     {% endblock %}
     {% block head_js %}
--- personal_site/templates/flask_index.py
+++ personal_site/templates/flask_index.py
@@ -1,4 +1,27 @@
 {% extends 'base_file.py' %}
+
+{% block meta %}
+	<!-- Facebook -->
+	<meta property="og:url" content="http://rafaelcenzano.com/">
+	<meta property="og:image" content="{{ url_for('static', filename='img/profile.jpeg') }}">
+	<meta property="og:description" content="Personal website to display Rafael Cenzano's work">
+	<meta property="og:title" content="Rafael Cenzano">
+	<meta property="og:site_name" content="Rafael Cenzano's Personal website">
+	<meta property="og:see_also" content="http://rafaelcenzano.com">
+
+	<!-- Twitter -->
+	<meta name="twitter:card" content="Rafael Cenzano's Personal website">
+	<meta name="twitter:url" content="http://rafaelcenzano.com/">
+	<meta name="twitter:title" content="Rafael Cenzano">
+	<meta name="twitter:description" content="Personal website to display Rafael Cenzano's work">
+	<meta name="twitter:image" content="{{ url_for('static', filename='img/profile.jpeg') }}">
+
+	<!-- Google+ -->
+	<meta itemprop="name" content="Rafael Cenzano">
+	<meta itemprop="description" content="Personal website to display Rafael Cenzano's work">
+	<meta itemprop="image" content="{{ url_for('static', filename='img/profile.jpeg') }}">
+{% endblock %}
+
 {% block content %}
   <h1>hi</h1>
 {% endblock %}
