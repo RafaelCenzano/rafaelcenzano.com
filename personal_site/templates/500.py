36533201739bcda1f8243888f1f0b251af1e37f9
Savagecoder77 <savagecoder77@gmail.com>
2019-03-13 17:58:24 -0700

    500 error links and pages

---
+++ personal_site/templates/500.py
@@ -0,0 +1,20 @@
+{% extends 'base_file.py' %}
+
+{% block title %}Server Error{% endblock %}
+
+{% block meta %}
+
+	<!-- Facebook -->
+	<meta property="og:url" content="https://www.rafaelcenzano.me/500">
+
+	<!-- Twitter -->
+	<meta name="twitter:url" content="https://www.rafaelcenzano.me/500">
+
+{% endblock %}
+
+{% block head_css %}
+    <link rel="stylesheet" type="text/css" href={{ url_for('static', filename='css/main/500.css') }}>
+{% endblock %}
+
+{% block content %}
+{% endblock %}
