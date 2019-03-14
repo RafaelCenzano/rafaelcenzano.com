70540c059d695f65cfa376340630da1211db3326
Savagecoder77 <savagecoder77@gmail.com>
2019-03-13 17:27:40 -0700

    change meta data

--- personal_site/templates/flask_index.py
+++ personal_site/templates/flask_index.py
@@ -1,27 +1,15 @@
 {% extends 'base_file.py' %}
 
-{% block title %}Rafael Cenzano{% endblock %}
+{% block title %}Rafael Cenzano - Raf{% endblock %}
 
 {% block meta %}
+
 	<!-- Facebook -->
-	<meta property="og:url" content="http://rafaelcenzano.com/">
-	<meta property="og:image" content="{{ url_for('static', filename='img/profile.jpeg') }}">
-	<meta property="og:description" content="Personal website to display Rafael Cenzano's work">
-	<meta property="og:title" content="Rafael Cenzano">
-	<meta property="og:site_name" content="Rafael Cenzano's Personal website">
-	<meta property="og:see_also" content="http://rafaelcenzano.com">
+	<meta property="og:url" content="https://www.rafaelcenzano.me/">
 
 	<!-- Twitter -->
-	<meta name="twitter:card" content="Rafael Cenzano's Personal website">
-	<meta name="twitter:url" content="http://rafaelcenzano.com/">
-	<meta name="twitter:title" content="Rafael Cenzano">
-	<meta name="twitter:description" content="Personal website to display Rafael Cenzano's work">
-	<meta name="twitter:image" content="{{ url_for('static', filename='img/profile.jpeg') }}">
+	<meta name="twitter:url" content="https://www.rafaelcenzano.me/">
 
-	<!-- Google+ -->
-	<meta itemprop="name" content="Rafael Cenzano">
-	<meta itemprop="description" content="Personal website to display Rafael Cenzano's work">
-	<meta itemprop="image" content="{{ url_for('static', filename='img/profile.jpeg') }}">
 {% endblock %}
 
 {% block head_css %}
