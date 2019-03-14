1dbee92e43779e58936f05297f3868cf0c7ec900
Savagecoder77 <savagecoder77@gmail.com>
2019-03-13 18:56:07 -0700

    add imports

--- personal_site/views.py
+++ personal_site/views.py
@@ -1,7 +1,7 @@
 from personal_site import app
 from json import load, dump # parse and add json data
 import urllib.request
-from flask import render_template, request, make_response, redirect, session, url_for
+from flask import render_template, request, make_response, redirect, session, url_for, send_file
 
 # Views
 @app.route("/", methods=['GET'])
