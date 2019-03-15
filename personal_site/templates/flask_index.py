5fb205ac831248eccd63d033cd221a3c4d08ad01
Savagecoder77 <savagecoder77@gmail.com>
2019-03-14 21:56:28 -0700

    add glitch name and image effects

--- personal_site/templates/flask_index.py
+++ personal_site/templates/flask_index.py
@@ -16,16 +16,20 @@
 
     <link rel="stylesheet" type="text/css" href={{ url_for('static', filename='css/main/home.css') }}>
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:100">
 
 {% endblock %}
 
 {% block content %}
 
-    <h1>Rafael Cenzano</h1>
-    <img src={{ url_for('static', filename='img/profile.jpeg') }} alt="Rafael Cenzano Profile Image" height="200" width="200">
-	<a href="https://github.com/RafaelCenzano"><i class="fa fa-github" aria-hidden="true"></i></a>
-	<a href="https://www.linkedin.com/in/rafael-cenzano/"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-  	<a href="https://www.facebook.com/profile.php?id=100008046498255"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+	<div id="app">
+		<div id="glitch-wrapper">
+			<!-- Make sure data-text equals the text you put inside the tags. -->
+			<h1 class="glitch" data-text="Rafael Cenzano">Rafael Cenzano</h1>
+		</div>
+	</div>
+	<div class="profile-wrapper">
+    	<img src={{ url_for('static', filename='img/profile.jpeg') }} alt="Rafael Cenzano's Profile Image" class="profile">
+	</div>
 
 {% endblock %}
