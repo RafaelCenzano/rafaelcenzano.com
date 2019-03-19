7221118414a3bc7bad7be4dde2687e496dc16c03
Savagecoder77 <savagecoder77@gmail.com>
2019-03-19 13:24:46 -0700

    add new config

--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 # Import os
 import os
+import secret
 
 SECRET_KEY = str(os.urandom(64))
 WTF_CSRF_ENABLED = True
 STATIC_FOLDER = 'static'
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 465
+MAIL_USERNAME = 'contact@lowelldev.club'
+MAIL_PASSWORD = secret.mail
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
