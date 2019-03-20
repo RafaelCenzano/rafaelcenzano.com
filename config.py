bc199bde5c302c59137d5b4eef7c6a2c459944d1
Savagecoder77 <savagecoder77@gmail.com>
2019-03-20 13:14:35 -0700

    save in enviorment

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 # Import os
 import os
-import secret
 
 SECRET_KEY = str(os.urandom(64))
 WTF_CSRF_ENABLED = True
@@ -8,6 +7,6 @@ STATIC_FOLDER = 'static'
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USERNAME = 'contact@lowelldev.club'
-MAIL_PASSWORD = secret.mail
+MAIL_PASSWORD = os.environ['FLASK_MAIL_PASSWORD']
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
