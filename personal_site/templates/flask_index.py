8f6f655fc4aa34875d4b031922e886ac3245d071
Savagecoder77 <savagecoder77@gmail.com>
2019-03-20 21:59:33 -0700

    remove dark button test

--- personal_site/templates/flask_index.py
+++ personal_site/templates/flask_index.py
@@ -23,8 +23,4 @@
 {% block content %}
 	<h1>RAFAEL</h1>
 	<h1>CENZANO</h1>
-
-  	<button type="button" name="dark_light" onclick="toggleDarkLight()" title="Toggle dark/light mode">🌛</button>
-
-  	<p>Just press the button above to toggle!</p>
 {% endblock %}
