ad0c8fc9ed9fe450d87c6a17b97e3ef7c135fe08
Savagecoder77 <savagecoder77@gmail.com>
2019-03-20 21:57:46 -0700

    recreate own css effects and html

--- personal_site/templates/flask_index.py
+++ personal_site/templates/flask_index.py
@@ -16,30 +16,15 @@
 
     <link rel="stylesheet" type="text/css" href={{ url_for('static', filename='css/main/home.css') }}>
 
-	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:100">
+	<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
 
 {% endblock %}
 
 {% block content %}
+	<h1>RAFAEL</h1>
+	<h1>CENZANO</h1>
 
-	<div id="app">
-		<div id="glitch-wrapper">
-			<!-- Make sure data-text equals the text you put inside the tags. -->
-			<h1 class="glitch" data-text="Rafael">Rafael</h1><br />
-			<h1 class="glitch" data-text="Cenzano">Cenzano</h1><br />
-		</div>
-	</div>
-	<div class="profile-wrapper">
-		<div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-			<div class="flipper">
-				<div class="front">
-		    		<img src={{ url_for('static', filename='img/profile.jpeg') }} alt="Rafael Cenzano's Profile Image" class="profile">
-		    	</div>
-				<div class="back">
-		    		<img src={{ url_for('static', filename='img/python_logo.png') }} alt="Rafael Cenzano's Profile Image" class="profile">
-				</div>
-			</div>
-		</div>
-	</div>
+  	<button type="button" name="dark_light" onclick="toggleDarkLight()" title="Toggle dark/light mode">🌛</button>
 
+  	<p>Just press the button above to toggle!</p>
 {% endblock %}
