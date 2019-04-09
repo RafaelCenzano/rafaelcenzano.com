1d832e64997989fd37e0a7f8fe26456c88cadb76
Savagecoder77 <savagecoder77@gmail.com>
2019-04-08 19:39:20 -0700

    new 404 page

---
+++ personal_site/templates/404.py
@@ -0,0 +1,40 @@
+{% extends 'base_file.py' %}
+
+{% block title %}404 not found{% endblock %}
+
+{% block meta %}
+
+	<!-- Facebook -->
+	<meta property="og:url" content="https://www.rafaelcenzano.me/404">
+
+	<!-- Twitter -->
+	<meta name="twitter:url" content="https://www.rafaelcenzano.me/404">
+
+{% endblock %}
+
+{% block head_css %}
+
+    <link rel="stylesheet" type="text/css" href={{ url_for('static', filename='css/main/404.css') }}>
+
+{% endblock %}
+
+{% block content %}
+	<div id="background-wrap">
+   		<div class="x1">
+        	<div class="cloud"></div>
+    	</div>
+    	<div class="x2">
+        	<div class="cloud"></div>
+    	</div>
+    	<div class="x3">
+        	<div class="cloud"></div>
+    	</div>
+    	<div class="x4">
+        	<div class="cloud"></div>
+    	</div>
+    	<div class="x5">
+        	<div class="cloud"></div>
+    	</div>
+	</div>
+	<h1>404</h1>
+{% endblock %}
