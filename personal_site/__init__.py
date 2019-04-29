106a9d9c5fae414f7f2bdd94a4c75e8b3cbbf4d4
Savagecoder77 <savagecoder77@gmail.com>
2019-04-28 23:17:27 -0700

    add new ping function to keep flask server online

--- personal_site/__init__.py
+++ personal_site/__init__.py
@@ -1,6 +1,19 @@
 # Flask imports
 from flask import Flask
 from flask_mail import Mail
+from time import sleep as delay
+import threading
+import urllib.request
+
+def self_ping():
+	while True:
+		# 5 minutes delay
+		delay(60*5)
+		x = urllib.request.urlopen('https://www.rafaelcenzano.me')
+
+ping_self = threading.Thread(target=self_ping, name='self-ping', daemon=True)
+
+ping_self.start()
 
 # Create Flask app
 app = Flask(__name__)
