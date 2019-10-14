2280ef0da471b86d9bd5bd5481ef816812a48dfe
Savagecoder77 <savagecoder77@gmail.com>
2019-10-14 14:37:09 -0700

    remove mail

--- personal_site/__init__.py
+++ personal_site/__init__.py
@@ -1,25 +1,8 @@
 # Flask imports
 from flask import Flask
-from flask_mail import Mail
-from time import sleep as delay
-import threading
-import urllib.request
-
-def self_ping():
-	while True:
-		# 5 minutes delay
-		delay(60*5)
-		x = urllib.request.urlopen('https://www.rafaelcenzano.me')
-
-ping_self = threading.Thread(target=self_ping, name='self-ping', daemon=True)
-
-ping_self.start()
 
 # Create Flask app
 app = Flask(__name__)
-
-# Create Mail app
-mail = Mail(app)
 
 # Add Configurations to app
 app.config.from_pyfile('config.py', silent=True)
