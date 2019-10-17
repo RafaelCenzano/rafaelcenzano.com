a9111b0eed5bd57f4d5599e1085bef0d9a270612
Savagecoder77 <savagecoder77@gmail.com>
2019-10-16 19:18:35 -0700

    remove extra meta data ❌

--- personal_site/templates/500.py
+++ personal_site/templates/500.py
@@ -2,16 +2,6 @@
 
 {% block title %}Server Error{% endblock %}
 
-{% block meta %}
-
-	<!-- Facebook -->
-	<meta property="og:url" content="https://www.rafaelcenzano.me/500">
-
-	<!-- Twitter -->
-	<meta name="twitter:url" content="https://www.rafaelcenzano.me/500">
-
-{% endblock %}
-
 {% block head_css %}
 
     <link rel="stylesheet" type="text/css" href={{ url_for('static', filename='css/main/500.css') }}>
