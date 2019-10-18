79519601e96d45f48538b0d1d10fea24dbd80279
Savagecoder77 <savagecoder77@gmail.com>
2019-10-18 13:07:17 -0700

    add new things

--- personal_site/templates/flask_index.py
+++ personal_site/templates/flask_index.py
@@ -17,30 +17,30 @@
 {% block content %}
 
 	<article class="color-changer">
-  		<h1 class="top-title">RAFAEL</h1>
+        <h1 class="top-title">RAFAEL</h1>
         <h1 class="top-title">CENZANO</h1>
-		<img src={{ url_for('static', filename='img/rafael_profile_square.jpeg') }} alt="Rafael Cenzano's profile image" class="profile">
-	</article>
+        <img src="https://github.com/RafaelCenzano/rafaelcenzano.com/blob/master/personal_site/static/img/rafael_profile_square.jpeg?raw=true" alt="Rafael Cenzano's profile image" class="profile">
+    </article>
 
-	<div class="social-buttons">
-  		<a href="https://www.facebook.com/profile.php?id=100008046498255" class="social-buttons__button social-button social-button--facebook" target="_blank" aria-label="Facebook">
-    		<span class="social-button__inner">
-     			<i class="fab fa-facebook-f"></i>
-    		</span>
-  		</a>
-  		<a href="https://www.linkedin.com/in/rafael-cenzano/" class="social-buttons__button social-button social-button--linkedin" target="_blank" aria-label="LinkedIn">
-    		<span class="social-button__inner">
-     			<i class="fab fa-linkedin-in"></i>
-    		</span>
-  		</a>
-  		<a href="https://github.com/RafaelCenzano" class="social-buttons__button social-button social-button--github" target="_blank" aria-label="GitHub">
-    		<span class="social-button__inner">
-     			<i class="fab fa-github"></i>
-    		</span>
-  		</a>
-	</div>
+    <div class="social-buttons">
+        <a href="https://www.facebook.com/profile.php?id=100008046498255" class="social-buttons__button social-button social-button--facebook" target="_blank" aria-label="Facebook">
+            <span class="social-button__inner">
+                <i class="fab fa-facebook-f"></i>
+            </span>
+        </a>
+        <a href="https://www.linkedin.com/in/rafael-cenzano/" class="social-buttons__button social-button social-button--linkedin" target="_blank" aria-label="LinkedIn">
+            <span class="social-button__inner">
+                <i class="fab fa-linkedin-in"></i>
+            </span>
+        </a>
+        <a href="https://github.com/RafaelCenzano" class="social-buttons__button social-button social-button--github" target="_blank" aria-label="GitHub">
+            <span class="social-button__inner">
+                <i class="fab fa-github"></i>
+            </span>
+        </a>
+    </div>
 
-	<div class="button-wrapper center">
+    <div class="button-wrapper center">
         <a href="mailto:contact@rafaelcenzano.com" class="contact">Contact me!</a>
     </div>
 
@@ -50,15 +50,15 @@
 
     <div class="projects things center">
         <div class="project-base">
-			<a href="https://github.com/Marvin-Virtual-Assistant/Marvin-V4" target="_blank">
-        	    <h3 class="project-title">Marvin Virtual Assistant</h3>
-        	    <p class="project-description things-description">
-        	    	A virtual assistant created in python that currently has 4 versions. This is a project that has tons of room to grow and will always be updated with hopes to have it be an open source project people want to contribute to.
-                    <b>
+            <a href="https://github.com/Marvin-Virtual-Assistant/Marvin-V4" target="_blank">
+                <h3 class="project-title">Marvin Virtual Assistant</h3>
+                <p class="project-description things-description">
+                    A virtual assistant created in python that currently has 4 versions. This is a project that has tons of room to grow and will always be updated with hopes to have it be an open source project people want to contribute to.
+                    <br><br>
                     Currently attempting to use Java for GUI and Python for ML and general computation.
-        	    </p>
-       		</a>
-       	</div>
+                </p>
+            </a>
+        </div>
     </div>
 
     <div class="title-wrapper">
@@ -69,13 +69,13 @@
         <h4><a href="https://www.hackthefog.com" target="_blank" class="experience">Hack The Fog (HTF)</a></h4>
         <p class="experience-description">Organized Hack The Fog the first high school hackathon in San Francisco and Organizing Hack the Fog 2.0.</p>
         <p class="current-experience-description experience-description">
-        	<a class="experience-link" target="_blank" href="https://www.hackthefog.com">~ Currently Co-Lead Organizer ~</a>
+            <a class="experience-link" target="_blank" href="https://www.hackthefog.com">~ Currently Co-Lead Organizer ~</a>
         </p>
         <p class="current-experience-description experience-description">
             <a class="experience-link" target="_blank" href="https://www.hackthefog.com">~ Organizer 1 year 4 months ~</a>
         </p>
         <p class="current-experience-description experience-description">
-        	<a class="experience-link" target="_blank" href="https://www.hackthefog.com">~ Operations 6 months ~</a>
+            <a class="experience-link" target="_blank" href="https://www.hackthefog.com">~ Operations 6 months ~</a>
         </p>
         <p class="current-experience-description experience-description">
             <a class="experience-link" target="_blank" href="https://www.sfchronicle.com/bayarea/article/Hack-the-Fog-makes-history-as-San-12729895.php">~ In a chronicle article!~</a>
@@ -86,7 +86,7 @@
         <h4><a href="https://jcyc.org/jyl.htm" target="_blank" class="experience">Japantown Youth Leader (JYL)</a></h4>
         <p class="experience-description">Youth Leadership program in Japantown San Francisco. We go into the community and voulnteer to help the community as well as learn valuable leadership skills.</p>
         <p class="current-experience-description experience-description">
-        	<a class="experience-link" target="_blank" href="https://jcyc.org/jyl.htm">~ Japantown Youth Leader 2 years + Currently in program ~</a>
+            <a class="experience-link" target="_blank" href="https://jcyc.org/jyl.htm">~ Japantown Youth Leader 2 years + Currently in program ~</a>
         </p>
         <p class="current-experience-description experience-description">
             <a class="experience-link" target="_blank" href="https://jcyc.org/jyl.htm">~ 224.5 volunteer hours total ~</a>
@@ -97,7 +97,7 @@
         <h4><a href="https://www.lowelldev.club/" target="_blank" class="experience">Lowell Dev Club</a></h4>
         <p class="experience-description">Programming club at Lowell High School. Our mission is to make people interested in programming and have amazing experiences that stay with them for their whole life.</p>
         <p class="current-experience-description experience-description">
-        	<a class="experience-link" target="_blank" href="https://www.lowelldev.club">~ Co-Founder and Current Co-Leader ~</a>
+            <a class="experience-link" target="_blank" href="https://www.lowelldev.club">~ Co-Founder and Current Co-Leader ~</a>
         </p>
     </div>
     <br>
@@ -105,10 +105,10 @@
         <h4><a href="#byte_lab_website_is_nonexsistant" class="experience">Byte Lab</a></h4>
         <p class="experience-description">Programming club at Lowell High School.</p>
         <p class="current-experience-description experience-description">
-        	<a class="experience-link" href="#byte_lab_website_is_nonexsistant">~ Club Secretary 1 year ~</a>
+            <a class="experience-link" href="#byte_lab_website_is_nonexsistant">~ Club Secretary 1 year ~</a>
         </p>
         <p class="current-experience-description experience-description">
-        	<a class="experience-link" href="#byte_lab_website_is_nonexsistant">~ Club Member 1 year ~</a>
+            <a class="experience-link" href="#byte_lab_website_is_nonexsistant">~ Club Member 1 year ~</a>
         </p>
     </div>
     <footer>
