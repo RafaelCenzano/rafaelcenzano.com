d5fc4ddf33f8f726565f99f63b36989e16e45259
Savagecoder77 <savagecoder77@gmail.com>
2019-10-20 09:27:05 -0700

    fix comments and rename functions

--- personal_site/views.py
+++ personal_site/views.py
@@ -1,23 +1,31 @@
+# Imports
 from personal_site import app
 from flask import render_template, request, make_response, redirect, session, url_for, send_file
 
-# Views
+'''
+Views
+'''
 @app.route("/", methods=['GET'])
 def index():
     return render_template('flask_index.py')
 
+
 @app.route("/404", methods=['GET'])
-def error_404():
+def notFoundError():
     return render_template('404.py'), 404
 
+
 @app.route("/500", methods=['GET'])
-def server_error():
+def serverError():
     return render_template('500.py'), 500
 
-# SEO
+'''
+SEO
+'''
 @app.route('/robots.txt', methods=['GET'])
 def robots():
     return send_file('templates/seo/robots.txt')
+
 
 @app.route('/sitemap.xml', methods=['GET'])
 def sitemap():
@@ -26,12 +34,14 @@ def sitemap():
     response.headers["Content-Type"] = "application/xml"
     return response
 
-# Error Handelers
+'''
+Error Handelers
+'''
 @app.errorhandler(404)
 def page_not_found(e):
-    return redirect(url_for('error_404'))
+    return redirect(url_for('notFoundError'))
 
-# Error Handelers
+
 @app.errorhandler(500)
 def server_error(e):
-    return redirect(url_for('server_error'))
+    return redirect(url_for('serverError'))
