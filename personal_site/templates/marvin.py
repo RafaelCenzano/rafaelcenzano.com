531ed93a93b621b969ebb1c54b60b0bec2eb7118
Savagecoder77 <savagecoder77@gmail.com>
2020-02-12 20:00:38 -0800

    new content for project page of Marvin

--- personal_site/templates/marvin.py
+++ personal_site/templates/marvin.py
@@ -26,3 +26,73 @@
 {% endblock %}
 
 {% block content %}
+
+    <div class="body-wrapper">
+        <article class="color-changer">
+            <h1 class="top-title">AP JAVA</h1>
+        </article>
+        <br>
+        <div class="button-wrapper center">
+            <a href="{{ url_for('index') }}" class="contact">Home</a>
+        </div>
+        <br>
+        <div class="experiences center things">
+            <h4><a href="https://github.com/Marvin-Virtual-Assistant" class="experience">MVA</a></h4>
+            <p class="current-experience-description experience-description">
+                <a class="experience-link" href="https://github.com/Marvin-Virtual-Assistant">Marvin Virtual Asssitant is a Github organization I own. I have brought some friends into the organization and they have helped with a few things through out the different versions of Marvin.</a>
+            </p>
+        </div>
+        <br>
+        <div class="experiences center things">
+            <h4><a href="https://github.com/Marvin-Virtual-Assistant/Marvin-V4" class="experience">V4</a></h4>
+            <p class="current-experience-description experience-description">
+                <a class="experience-link" href="https://github.com/Marvin-Virtual-Assistant/Marvin-V4">Version 4.0 of Marvin. Version 4 has a Flask app for the GUI and utilizes more advanced coding techniques to make code more effiecient. It has been devloped a ton on ceratin features but this is still much work to be done to regain and refactor past features of Marvin projects.</a>
+            </p>
+        </div>
+        <br>
+        <div class="experiences center things">
+            <h4><a href="https://github.com/Marvin-Virtual-Assistant/notebooks" class="experience">Notebooks</a></h4>
+            <p class="current-experience-description experience-description">
+                <a class="experience-link" href="https://github.com/Marvin-Virtual-Assistant/notebooks">Jupyter notebooks for Marvin version 4.0. I experiment and test code in jupyter notebooks which are published in a seperate repo to keep the repos cleaner and so testing and feature devlopment progress can be seen online.</a>
+            </p>
+        </div>
+        <br>
+        <div class="experiences center things">
+            <h4><a href="https://github.com/Marvin-Virtual-Assistant/Marvin-v3-client" class="experience">V3 - client</a></h4>
+            <p class="current-experience-description experience-description">
+                <a class="experience-link" href="https://github.com/Marvin-Virtual-Assistant/Marvin-v3-client">Version 3.0 of Marvin for clients. I attempted to create a client version of marvin that could work with the server version to create a better experience but due to the server version not working out this version in turned failed because it was initially built to work with the server version.</a>
+            </p>
+        </div>
+        <br>
+        <div class="experiences center things">
+            <h4><a href="https://github.com/Marvin-Virtual-Assistant/Marvin-v3-server" class="experience">V3 - server</a></h4>
+            <p class="current-experience-description experience-description">
+                <a class="experience-link" href="https://github.com/Marvin-Virtual-Assistant/Marvin-v3-server">Version 3.0 of Marvin in the cloud. I attempted to create flask app api that could be called by the client version but it was too slow and never worked like I had wished.</a>
+            </p>
+        </div>
+        <br>
+        <div class="experiences center things">
+            <h4><a href="https://github.com/Marvin-Virtual-Assistant/MARVIN_2.0" class="experience">V2</a></h4>
+            <p class="current-experience-description experience-description">
+                <a class="experience-link" href="https://github.com/Marvin-Virtual-Assistant/MARVIN_2.0">Version 2.0 of Marvin. I refactored a huge amount of the code and created many new features. It was a great success and allowed me to learn new strategies for coding and python code. It is on the inefficient side due to putting together code from many different sources.</a>
+            </p>
+        </div>
+        <br>
+        <div class="experiences center things">
+            <h4><a href="https://github.com/Marvin-Virtual-Assistant/Marvin" class="experience">V1</a></h4>
+            <p class="current-experience-description experience-description">
+                <a class="experience-link" href="https://github.com/Marvin-Virtual-Assistant/Marvin">My original marvin virtual assistant. It one of my <strong>first</strong> projects. It has simple python code allowing it to give users cool functions but due to my inexperience with coding at the time it is highly inefficient.</a>
+            </p>
+        </div>
+        <br>
+        <footer>
+            <p class="copyright">© 2020 Rafael Cenzano</p>
+            <div class="source-wrapper">
+                <p>
+                    <a href="https://github.com/RafaelCenzano/rafaelcenzano.com" class="source">Source</a>
+                </p>
+            </div>
+        </footer>
+    </div>
+
+{% endblock %}
