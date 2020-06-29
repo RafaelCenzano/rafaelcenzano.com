5daf81b7ddde869a16cc5a805ea13c5db209760d
RafaelCenzano <savagecoder77@gmail.com>
2020-06-29 01:54:53 -0700

    add rel="noopener"

--- personal_site/templates/footer.py
+++ personal_site/templates/footer.py
@@ -2,7 +2,7 @@
     <p class="copyright">© 2020 Rafael Cenzano</p>
     <div class="source-wrapper">
         <p>
-            <a href="https://github.com/RafaelCenzano/rafaelcenzano.com" class="source">Source</a>
+            <a rel="noopener" href="https://github.com/RafaelCenzano/rafaelcenzano.com" class="source">Source</a>
         </p>
     </div>
 </footer>
