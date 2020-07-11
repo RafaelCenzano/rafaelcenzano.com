bc5a086e955f9cf364deb8c3a525b1fdfcf4b9be
RafaelCenzano <savagecoder77@gmail.com>
2020-07-11 12:04:20 -0700

    correct images

--- personal_site/templates/base_file.py
+++ personal_site/templates/base_file.py
@@ -15,34 +15,34 @@
 	<link rel="author" href="https://rafaelcenzano.com">
 
 	<!-- Favicon -->
-	<link rel="icon" type="image/png" sizes="32x32" href="https://rafael.sirv.com/Images/faivon.png?w=32&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="64x64" href="https://rafael.sirv.com/Images/faivon.png?w=64&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="96x96" href="https://rafael.sirv.com/Images/faivon.png?w=96&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="100x100" href="https://rafael.sirv.com/Images/faivon.png?w=100&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="200x200" href="https://rafael.sirv.com/Images/faivon.png?w=200&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="300x300" href="https://rafael.sirv.com/Images/faivon.png?w=300&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="400x400" href="https://rafael.sirv.com/Images/faivon.png?w=400&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="500x500" href="https://rafael.sirv.com/Images/faivon.png?w=500&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="32x32" href="https://rafael.sirv.com/Images/favicon.png?w=32&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="64x64" href="https://rafael.sirv.com/Images/favicon.png?w=64&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="96x96" href="https://rafael.sirv.com/Images/favicon.png?w=96&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="100x100" href="https://rafael.sirv.com/Images/favicon.png?w=100&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="200x200" href="https://rafael.sirv.com/Images/favicon.png?w=200&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="300x300" href="https://rafael.sirv.com/Images/favicon.png?w=300&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="400x400" href="https://rafael.sirv.com/Images/favicon.png?w=400&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="500x500" href="https://rafael.sirv.com/Images/favicon.png?w=500&png.optimize=true">
 	<link rel="alternate icon" href="https://rafael.sirv.com/Images/favicon.ico">
 
 	<!-- Apple Icon -->
-	<link rel="apple-touch-icon" sizes="57x57"   href="https://rafael.sirv.com/Images/faivon.png?w=57&png.optimize=true">
-	<link rel="apple-touch-icon" sizes="60x60"   href="https://rafael.sirv.com/Images/faivon.png?w=60&png.optimize=true">
-	<link rel="apple-touch-icon" sizes="72x72"   href="https://rafael.sirv.com/Images/faivon.png?w=72&png.optimize=true">
-	<link rel="apple-touch-icon" sizes="76x76"   href="https://rafael.sirv.com/Images/faivon.png?w=76&png.optimize=true">
-	<link rel="apple-touch-icon" sizes="114x114" href="https://rafael.sirv.com/Images/faivon.png?w=114&png.optimize=true">
-	<link rel="apple-touch-icon" sizes="120x120" href="https://rafael.sirv.com/Images/faivon.png?w=120&png.optimize=true">
-	<link rel="apple-touch-icon" sizes="144x144" href="https://rafael.sirv.com/Images/faivon.png?w=144&png.optimize=true">
-	<link rel="apple-touch-icon" sizes="152x152" href="https://rafael.sirv.com/Images/faivon.png?w=152&png.optimize=true">
-	<link rel="apple-touch-icon" sizes="180x180" href="https://rafael.sirv.com/Images/faivon.png?w=180&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="57x57"   href="https://rafael.sirv.com/Images/favicon.png?w=57&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="60x60"   href="https://rafael.sirv.com/Images/favicon.png?w=60&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="72x72"   href="https://rafael.sirv.com/Images/favicon.png?w=72&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="76x76"   href="https://rafael.sirv.com/Images/favicon.png?w=76&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="114x114" href="https://rafael.sirv.com/Images/favicon.png?w=114&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="120x120" href="https://rafael.sirv.com/Images/favicon.png?w=120&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="144x144" href="https://rafael.sirv.com/Images/favicon.png?w=144&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="152x152" href="https://rafael.sirv.com/Images/favicon.png?w=152&png.optimize=true">
+	<link rel="apple-touch-icon" sizes="180x180" href="https://rafael.sirv.com/Images/favicon.png?w=180&png.optimize=true">
 
 	<!-- Andriod Icon -->
-	<link rel="icon" type="image/png" sizes="36x36"    href="https://rafael.sirv.com/Images/faivon.png?w=36&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="48x48"    href="https://rafael.sirv.com/Images/faivon.png?w=48&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="72x72"    href="https://rafael.sirv.com/Images/faivon.png?w=72&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="96x96"    href="https://rafael.sirv.com/Images/faivon.png?w=96&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="144x144"  href="https://rafael.sirv.com/Images/faivon.png?w=144&png.optimize=true">
-	<link rel="icon" type="image/png" sizes="192x192"  href="https://rafael.sirv.com/Images/faivon.png?w=192&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="36x36"    href="https://rafael.sirv.com/Images/favicon.png?w=36&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="48x48"    href="https://rafael.sirv.com/Images/favicon.png?w=48&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="72x72"    href="https://rafael.sirv.com/Images/favicon.png?w=72&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="96x96"    href="https://rafael.sirv.com/Images/favicon.png?w=96&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="144x144"  href="https://rafael.sirv.com/Images/favicon.png?w=144&png.optimize=true">
+	<link rel="icon" type="image/png" sizes="192x192"  href="https://rafael.sirv.com/Images/favicon.png?w=192&png.optimize=true">
 
 	<!-- Facebook -->
 	<meta property="og:title" content="Rafael Cenzano">
